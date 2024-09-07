c9143c68594bdb69ecc6ca10620a312d093ed80f
laurent bernabé <laurent.bernabe@gmail.com>
2024-09-07 12:49:08 +0200

    set up icon for windows

---
+++ build.rs
@@ -0,0 +1,9 @@
+const ICON: &str = "icon.ico";
+
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon(ICON);
+        res.compile().unwrap();
+    }
+}
